47e230379dc888c7587638aa69563022d3ac0f63
miguel-l-b <miguellopesbraido@gmail.com>
2023-08-23 11:16:57 -0300

    config: `tailwind`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./screens/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.tsx",
+    "./src/pages/**/*.tsx",
     "./App.tsx"
   ],
   theme: {
