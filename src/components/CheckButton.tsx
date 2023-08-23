f6b12562b17469d97f36c08bc5a36f2ba6096310
miguel-l-b <cc22327@g.unicamp.br>
2023-08-23 16:58:53 -0300

    feat: create a button `checkButton`

--- src/components/CheckButton.tsx
+++ src/components/CheckButton.tsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import { GestureResponderEvent, TouchableOpacity, View } from "react-native"
 
 export interface FloatButtonProps {
   isChecked?: boolean;
-  onPress: (e: GestureResponderEvent) => void;
+  alterValue: (value: boolean) => void;
 }
 
-export default function CheckButton({ isChecked, onPress }: FloatButtonProps) {
+export default function CheckButton({ isChecked, alterValue }: FloatButtonProps) {
+  const [checked, setChecked] = useState(isChecked || false);
   return (
-    <TouchableOpacity className={`w-10 h-10 rounded-xl border-4 bg-gray-400 border-gray-700 p-1`}>
-      <View className="bg-gray-800 rounded-lg w-full h-full" />
+    <TouchableOpacity onPress={(e) => { setChecked(!checked); alterValue(checked) }} className={`w-10 h-10 rounded-xl border-4 bg-gray-400 border-gray-700 p-1`}>
+      <View className={`${checked && "bg-gray-800"} rounded-lg w-full h-full`} />
     </TouchableOpacity>
   );
 }
