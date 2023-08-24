2d65eb696c2744165c46129a8e922eea45875fa4
miguel-l-b <cc22327@g.unicamp.br>
2023-08-24 18:19:35 -0300

    fix: sobresaindo dropbox

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,7 +9,7 @@ export interface InputProps {
 export default function Input(props: InputProps) {
     const text_aling = "text-" + props.direction || "start"
     return (
-        <View className="bg-gray-400 w-full rounded-xl px-2 py-4">
+        <View className="bg-gray-400 w-full -z-10 rounded-2xl px-2 py-4">
             <TextInput className={`w-full text-base color-gray-700 ${text_aling}`} onChangeText={(e) => props.setValue(e)} placeholder={props.placeholder} />
         </View>
     )
