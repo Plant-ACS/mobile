c205672907cfbd91a73a02d8924063f42eb86b48
miguel-l-b <cc22327@g.unicamp.br>
2023-08-24 18:19:05 -0300

    fix: error in colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,28 +2,26 @@ import { TouchableOpacity, Text, View } from "react-native"
 
 export interface ButtonProps {
     text: string,
-    theme: "primary" | "secondary",
+    theme?: "primary" | "secondary",
     isHover?: boolean,
     onPress: () => void,
     children?: React.ReactNode
 }
 
 export default function Button(props: ButtonProps) {
-    let styleBg
-    let styleText
-    if (props.isHover === true) {
+    const colorBg = props.theme === "secondary"? 
+        props.isHover? "blue-900" : "blue-200"
+        :
+        props.isHover? "green-700" : "green-200"
 
-        styleBg = props.theme === 'primary' ? 'green-700' : 'blue-900'
-        styleText = props.theme === 'primary' ? 'green-200' : 'blue-200'
-    }
-    else {
-        styleBg = props.theme === 'primary' ? 'green-200' : 'blue-200'
-        styleText = props.theme === 'primary' ? 'green-700' : 'blue-900'
-    }
+    const colorText = props.theme === "secondary"?
+        props.isHover? "blue-200" : "blue-900"
+        :
+        props.isHover? "green-200" : "green-700"
     return (
-        <TouchableOpacity className={`w-full rounded-xl px-2 py-4 bg-${styleBg} items-center shadow-md shadow-${styleText}`}>
-            <View>{props.children}</View>
-            <Text className={`w-full text-${styleText} text-center text-base`}>{props.text}</Text>
+        <TouchableOpacity className={`relative -z-10 w-full rounded-xl px-4 py-4 ${"bg-"+colorBg} items-center shadow-md ${"shadow-"+colorText}`}>
+            <View className="absolute left-4 bottom-4">{props.children}</View>
+            <Text className={`w-full ${"text-"+colorText} text-center text-base`}>{props.text}</Text>
         </TouchableOpacity>
     )
 }
