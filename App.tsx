13b5499e46c5cc86c91352786de5d6e0f3bf554b
miguel-l-b <miguellopesbraido@gmail.com>
2023-09-06 12:16:47 -0300

    feat: add font `Quicksand`

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar'
+import { useEffect } from "react"
 import { View } from 'react-native'
 import { colors } from './src/styles'
-import Loader from '@pages/Loader'
+import Home from '@pages/Home'
+import { useFonts } from "expo-font"
+import { hideAsync, preventAutoHideAsync } from 'expo-splash-screen'
 
+preventAutoHideAsync();
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Quicksand": require("./assets/fonts/Quicksand.ttf"),
+    "Quicksand-Bold": require("./assets/fonts/Quicksand_Bold.otf")
+  })
+
+  useEffect(() => {
+    if(fontsLoaded)
+      hideAsync()
+  }, [fontsLoaded])
+
+  if(!fontsLoaded)
+    return null
+
   return (
-    <View style={{
-      flex: 1,
-      width: "100%",
-      height: "100%",
-      paddingTop: 40,
-      backgroundColor: colors["white-900"],
-    }}>
-      <Loader />
-      <StatusBar style="dark" backgroundColor={colors["white-900"]} />
-    </View>
+    <>
+      <StatusBar style="dark" backgroundColor="#F0F0F0" />
+      <View style={{
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        paddingTop: 38,
+        backgroundColor: colors["white-900"],
+      }}>
+        <Home />
+      </View>
+    </>
   );
 }
 
--- src/styles/texts.ts
+++ src/styles/texts.ts
@@ -3,58 +3,66 @@ import { colors } from "./colors"
 
 export const textsStyle = StyleSheet.create({
   title: {
-    fontSize: 42,
-    fontWeight: "500",
+    fontFamily: "Quicksand-Bold",
+    fontSize: 35,
     color: colors["black-900"],
   },
   title_2: {
-    fontSize: 35,
-    fontWeight: "500",
+    fontFamily: "Quicksand-Bold",
+    fontSize: 30,
     color: colors["black-900"],
   },
   title_3: {
-    fontSize: 30,
-    fontWeight: "500",
+    fontFamily: "Quicksand-Bold",
+    fontSize: 23,
     color: colors["black-900"],
   },
 
   label: {
+    fontFamily: "Quicksand",
     fontSize: 22.5,
     fontWeight: "300",
     color: colors["black-900"]
   },
 
   subtitle: {
-    fontSize: 26,
-    fontWeight: "300",
-    color: colors["black-900"],
-  },
-  subtitle_2: {
+    fontFamily: "Quicksand",
     fontSize: 23,
     fontWeight: "300",
     color: colors["black-900"],
   },
-  subtitle_3: {
+  subtitle_2: {
+    fontFamily: "Quicksand",
     fontSize: 20,
     fontWeight: "300",
     color: colors["black-900"],
   },
-  text_xl: {
-    fontSize: 20,
-    fontWeight: "400",
+  subtitle_3: {
+    fontFamily: "Quicksand",
+    fontSize: 18,
+    fontWeight: "300",
     color: colors["black-900"],
   },
-  text: {
+  text_xl: {
+    fontFamily: "Quicksand",
     fontSize: 18,
     fontWeight: "400",
     color: colors["black-900"],
   },
-  text_sm: {
+  text: {
+    fontFamily: "Quicksand",
     fontSize: 16,
     fontWeight: "400",
     color: colors["black-900"],
   },
+  text_sm: {
+    fontFamily: "Quicksand",
+    fontSize: 12,
+    fontWeight: "400",
+    color: colors["black-900"],
+  },
   link: {
+    fontFamily: "Quicksand",
     color: colors["blue-500"],
     fontWeight: "200",
   }
