ef9ac7abf278df406fce7489cdb5a5ddfe209210
Gustavo Sousa <gustavosousa.adm@hotmail.com>
2023-09-20 22:29:37 -0300

    fix : Adding Google icon in the button

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import Button from "@components/Button"
 import Input from "@components/Input"
 import { MaterialIcons, MaterialCommunityIcons} from "@expo/vector-icons"
 import { textsStyle, viewsStyle } from "../styles"
+import GoogleIcon from "@components/GoogleIcon"
 
 export default function Login() {
     return(
@@ -22,7 +23,7 @@ export default function Login() {
                 <Button text="Entrar" isHover={true} theme="primary" onPress={()=> console.log("press enter")}></Button>
                 <Button text="Cadastrar-se" isHover={false} theme="primary" onPress={()=> console.log("press Register")}></Button>
                 <Button text="Google" isHover={false} theme="primary" onPress={()=> console.log("press Google")}>
-                    <MaterialCommunityIcons name="google" size={30} color={"#3F3D3B"} />
+                    <GoogleIcon/>
                 </Button>
             </View>
         </View>
