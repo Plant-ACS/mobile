f1d1197d050ace4668650f337b87eea93977c212
Gustavo Sousa <gustavosousa.adm@hotmail.com>
2023-09-25 17:27:11 -0300

    fix : Adding type to input text

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,10 @@
-import { TextInput, View } from "react-native"
+import { KeyboardTypeOptions, TextInput, View } from "react-native"
 import { inputsStyle } from "../styles"
 
 export interface InputProps {
     placeholder?: string,
     direction?: "start" | "center",
+    type?: KeyboardTypeOptions,
     setValue: (value: string) => void,
 }
 
@@ -11,7 +12,7 @@ export default function Input(props: InputProps) {
     const text_align = props.direction || "start"
     return (
         <View style={[inputsStyle.body]}>
-            <TextInput style={[inputsStyle.text]} onChangeText={(e) => props.setValue(e)} placeholder={props.placeholder} />
+            <TextInput keyboardType={props.type} style={[inputsStyle.text]} onChangeText={(e) => props.setValue(e)} placeholder={props.placeholder} />
         </View>
     )
 }
