ec7643f604a15def37b22206a69bc85fc54f5a24
miguel-l-b <miguellopesbraido@gmail.com>
2023-10-12 12:42:35 -0300

    feat(continue): crete a page `Register`

---
+++ src/pages/Register.tsx
@@ -0,0 +1,10 @@
+import { viewsStyle } from "@styles/views";
+import { View, Text } from "react-native";
+
+export default function Register() {
+  return (
+      <View style={viewsStyle.form}>
+          
+      </View>
+  )
+}
