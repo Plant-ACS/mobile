563627ea474234c343000d454edf653166b3472c
miguel-l-b <miguellopesbraido@gmail.com>
2023-10-12 12:41:11 -0300

    ref: performing navigation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,13 @@
 import { Text, View } from "react-native"
 import Button from "@components/Button"
 import Input from "@components/Input"
-import { MaterialIcons, MaterialCommunityIcons} from "@expo/vector-icons"
 import { textsStyle, viewsStyle } from "../styles"
-import GoogleIcon from "@components/GoogleIcon"
+import { PageProps } from "../types"
 
-export default function Login() {
+export default function Login({...props}: PageProps) {
     return(
         <View style={viewsStyle.form}>
             <Text style={textsStyle.subtitle_3}>Intelligreen</Text>
-
             <View style={viewsStyle.group_inputs}>
                 <Text style={textsStyle.label}>Nome de Usuário</Text>
 
@@ -20,11 +18,8 @@ export default function Login() {
             </View>
 
             <View style={viewsStyle.group_buttons}>
-                <Button text="Entrar" isHover={true} theme="primary" onPress={()=> console.log("press enter")}></Button>
-                <Button text="Cadastrar-se" isHover={false} theme="primary" onPress={()=> console.log("press Register")}></Button>
-                <Button text="Google" isHover={false} theme="primary" onPress={()=> console.log("press Google")}>
-                    <GoogleIcon/>
-                </Button>
+                <Button text="Entrar" isHover onPress={() => props.navigation.navigate("", {})}></Button>
+                <Button text="Cadastrar-se" onPress={() => props.navigation.navigate("Register")}></Button>
             </View>
         </View>
     )
