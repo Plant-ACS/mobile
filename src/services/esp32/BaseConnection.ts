1d87f72bb8d5ac4812bdbee4ebcb6607ea6219de
miguel-l-b <miguellopesbraido@gmail.com>
2023-11-19 22:38:38 -0300

    ref: add characteristics array to BaseConnection

--- src/services/esp32/BaseConnection.ts
+++ src/services/esp32/BaseConnection.ts
@@ -3,6 +3,7 @@ import { BleManager, Characteristic, Device } from "react-native-ble-plx"
 export default class BaseConnection {
   private static bleManager = new BleManager()
   private static deviceConnected: Device | null = null
+  private static characteristics: Characteristic[][] = []
 
   public static async Connect(device: Device) {
     try {
@@ -10,45 +11,15 @@ export default class BaseConnection {
       await BaseConnection.bleManager.connectToDevice(device.id).then(async (device) => {
         this.deviceConnected = device
         await this.deviceConnected.discoverAllServicesAndCharacteristics()
-        while (!this.deviceConnected.isConnected());
-        console.log(
-          await new Promise((resolve, reject) => {
-                device.services().then(services => {
-                    const characteristics: Characteristic[][] = []
-
-                    services.forEach((service, i) => {
-                        service.characteristics().then(c => {
-                            characteristics.push(c)
-
-                            if (i === services.length - 1) {
-                                const temp = characteristics.reduce(
-                                    (acc, current) => {
-                                        return [...acc, ...current]
-                                    },
-                                    []
-                                )
-
-                                console.log(temp)
-
-                                const dialog = temp.find(
-                                    characteristic =>
-                                        characteristic.isWritableWithResponse
-                                )
-                                if (!dialog) {
-                                    reject('No writable characteristic')
-                                }
-                                resolve(dialog)
-                            }
-                        })
-                    })
-                })
-            })
-        )
-        this.deviceConnected.serviceUUIDs = await this.deviceConnected.services().then((services) => services.map((service) => service.uuid))
-        console.log("Connected to: " + this.deviceConnected.name+ " - " + this.deviceConnected.id+ " - " + this.deviceConnected.serviceUUIDs)
-        console.log(this.Read())
-      }
-      )
+        while (!this.deviceConnected.isConnected())
+        device.services()
+          .then( async (services) => {
+            for(const service of services)
+              service.characteristics().then((characteristic) => {
+                this.characteristics = [...this.characteristics, characteristic]
+              })
+          })
+        })
     } catch (error) {
       throw error
     }
@@ -56,19 +27,15 @@ export default class BaseConnection {
 
   public static async Send(data: Object): Promise<boolean> {
     try {
+      if (!this.deviceConnected)
+        throw new Error("No device connected")
+
+      if (!this.deviceConnected.isConnected())
+        throw new Error("Device is not connected")
       JSON.stringify(data).match(/.{1,20}/g)?.map(async (dataPacket) => {
-        if (!this.deviceConnected)
-          throw new Error("No device connected")
-
-        if (!this.deviceConnected.isConnected())
-          throw new Error("Device is not connected")
-
-        this.deviceConnected.writeCharacteristicWithResponseForService(
-          this.deviceConnected.serviceUUIDs!![0],
-          this.deviceConnected.serviceUUIDs!![1],
-          dataPacket
-        )                           
+        this.characteristics[0][0].writeWithResponse(JSON.stringify(data))
       })
+
       return true
     }
     catch (error) {
